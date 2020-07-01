0e579e415a15f1b71ca583a414b8070ccf576332
hiroki-ume <h.ume1219bknb@gmail.com>
2020-07-01 21:36:31 +0900

    [finish]rgb変換プログラムを作成する

---
+++ lib/rgb.rb
@@ -0,0 +1,18 @@
+def to_hex(r, g, b)
+  [r, g, b].inject("#") do |hex, n|
+    hex + n.to_s(16).rjust(2, "0")
+  end
+end
+
+def　to_ints(hex)
+  r, g, b = hex[1..2], hex[3..4], hex[5..6]
+
+  [r, g, b].map do |s|
+    s.hex
+  end
+end
+
+# 同じ動きをする
+# def to_ints_master(hex)
+#   r, g, b = hex.scan(/\w\w/).map(&:hex)
+# end
--- lib/sample.rb
+++ lib/sample.rb
@@ -1,5 +1,7 @@
-puts 1 + 2
-a = "Hello World"
-a
-b = "こんにちは"
-puts b
+sum = 0
+(1..4).each {|n| sum += n}
+puts sum
+
+numbers = []
+(1..10).step(2){ |n| numbers << n }
+puts numbers
