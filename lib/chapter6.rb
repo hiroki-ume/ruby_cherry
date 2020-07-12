6c51797c39d1fdde0ccd161208f94ab4bb69bc29
hiroki-ume <h.ume1219bknb@gmail.com>
2020-07-12 13:04:05 +0900

    [finish]chapter6

--- lib/chapter6.rb
+++ lib/chapter6.rb
@@ -31,3 +31,72 @@ TEXT
 replaced = html.gsub(/<option value="(\w+)"(?: selected)?>(.*)<\/option>/, '\1,\2')
 
 puts replaced
+
+puts "--------------------------------"
+
+if '123-456' =~ /\d{3}-\d{4}/
+  puts "マッチしました"
+else
+  puts "マッチしませんでした"
+end
+
+puts "--------------------------------"
+
+text = "私の誕生日は1995年12月19日です"
+if m = /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/ =~ text
+  puts "#{year}/#{month}/#{day}"
+else
+  puts "マッチしませんでした"
+end
+
+puts "--------------------------------"
+
+birthday = "1995年12月19日　1996年3月16日".scan(/(?:\d+年)(?:\d+月)(?:\d+日)/)
+puts birthday[0]
+
+puts "--------------------------------"
+
+text = "郵便番号は111-1111です 321-3333 543-5687"
+postcode = text.gsub("-", ":")
+puts postcode
+
+puts "--------------------------------"
+
+text = "0000/00/00"
+case text
+when /^\d{3}-\d{4}$/
+  puts "郵便番号です"
+when /^\d{4}\/\d{1,2}\/\d{1,2}$/
+  puts "日付です"
+when /^\d+-\d+-\d+$/
+  puts "電話番号です"
+end
+
+puts "--------------------------------"
+
+puts "hello\nbye" =~ /hello.bye/
+puts "hello\nbye" =~ /hello.bye/m
+
+puts "--------------------------------"
+
+regexp = /
+\d{3}
+-
+\d{4}
+/x
+
+puts "000-0000" =~ regexp
+
+puts "--------------------------------"
+
+text = "私の誕生日は1995年12月19日です"
+
+text =~ /(\d+)年(\d+)月(\d+)日/
+
+puts Regexp.last_match
+
+puts $1
+
+puts "--------------------------------"
+
+puts /\d{3}-\d{4}/.match?"000-000"
