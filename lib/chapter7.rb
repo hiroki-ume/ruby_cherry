524283d9e907da6d115262b562c390941d4a3e14
hiroki-ume <h.ume1219bknb@gmail.com>
2020-07-20 20:41:38 +0900

    [update]様々なクラスの定義方法

--- lib/chapter7.rb
+++ lib/chapter7.rb
@@ -14,6 +14,13 @@ class User
   def hello
     "Hello, I am #{@name}."
   end
+
+  # privateメソッド
+  private
+
+  def hello_private
+    puts "hello"
+  end
 end
 
 names = ["alice", "bob", "carol"]
@@ -38,3 +45,61 @@ end
 
 product = Product.new("free movie")
 puts product.price
+
+puts "---------------------------"
+
+class Foo
+  puts "クラス公文の直下のself:#{self}"
+
+  def self.bar
+    puts "クラスメソッド内のself:#{self}"
+  end
+
+  def baz
+    puts "インスタンスメソッド内のself:#{self}"
+  end
+end
+
+puts Foo.bar
+
+foo = Foo.new
+puts foo.baz
+
+puts "---------------------------"
+
+class Product
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  def self.format_price(price)
+    "#{price}円"
+  end
+
+  def to_s
+    puts "name:#{name}, price:#{price}"
+  end
+end
+
+class DVD < Product
+  attr_reader :running_time
+
+  def initialize(name, price, running_time)
+    super(name, price)
+    @running_time = running_time
+  end
+
+  def to_s
+    "#{super}, running_time: #{running_time}"
+  end
+end
+
+product = Product.new("a great movie", 1000)
+product.to_s
+dvd = DVD.new("a great movie", 100, 120)
+dvd.to_s
+
+puts "---------------------------"
